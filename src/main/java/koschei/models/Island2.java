f4630bc56576dc041692ea48fc1cf233d0ded89c
ixixiyuj <kozlowa.d@gmail.com>
2022-10-07 16:10:59 +0300

    0

--- src/main/java/koschei/models/Island2.java
+++ src/main/java/koschei/models/Island2.java
@@ -1,7 +1,13 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+
 public class Island2 {
 
+    @Autowired
     private Wood3 wood;
 
     public Island2(Wood3 wood) {
